664e4f11baaf88b68c5ddf3260e586bc8a7d1d6d
TripNRaVeR <noerialexeew@live.nl>
2012-07-15 18:11:24 +0200

    device_endeavoru: this doesnt need to be defined 2x

--- device_endeavoru.mk
+++ device_endeavoru.mk
@@ -257,16 +257,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
-# file that declares the MIFARE NFC constant
-PRODUCT_COPY_FILES += \
-	device/sample/nxp/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
-
-# NFC EXTRAS add-on API
-PRODUCT_PACKAGES += \
-	com.android.nfc_extras
-PRODUCT_COPY_FILES += \
-	frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
-
 # NFCEE access control
 ifeq ($(TARGET_BUILD_VARIANT),user)
     NFCEE_ACCESS_PATH := device/htc/endeavoru/nfcee_access.xml
